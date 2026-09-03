ac48e63bc451b130953c111c795628b44b80e47c
isavj <vjisamary@gmail.com>
2026-09-02 19:55:00 -0600

    Agregando carpeta UML con mis diagramas

---
+++ Practica3/src/practica3/Main3.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package practica3;
+
+/**
+ *
+ * @author admin
+ */
+public class Main3
+{
+
+    public static void main(String[] args) {
+        Practica3 practica3 = new Practica3();
+    }
+    
+}
